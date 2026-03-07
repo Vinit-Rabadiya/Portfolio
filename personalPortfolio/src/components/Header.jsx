fd5b119a884bac8cd391bd10f52d454ab79a3eef
vinit <vinitrabadiyasoft.en@gmail.com>
2026-03-07 21:43:25 +0300

    added home to header

--- personalPortfolio/src/components/Header.jsx
+++ personalPortfolio/src/components/Header.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { NavLink } from "react-router";
 
 function Header() {
-  return <h1>Header</h1>;
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-grey-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
