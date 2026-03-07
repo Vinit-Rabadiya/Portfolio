fe98b76b149c37c2eb9b5ee1c23fd9eef65bddb5
vinit <vinitrabadiyasoft.en@gmail.com>
2026-03-07 21:16:35 +0300

    added router and route

--- personalPortfolio/src/App.jsx
+++ personalPortfolio/src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router";
 
 function App() {
-
-
-  return( <>
-  </>
-)}
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
 
 export default App;
