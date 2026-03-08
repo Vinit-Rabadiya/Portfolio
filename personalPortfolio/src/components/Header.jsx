a37a79dd095b5fae9f60ca239c45e66e1e87731b
vinit <vinitrabadiyasoft.en@gmail.com>
2026-03-08 14:16:39 +0300

    added home logic

--- personalPortfolio/src/components/Header.jsx
+++ personalPortfolio/src/components/Header.jsx
@@ -1,13 +1,26 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate, useLocation } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleHomeClick = () => {
+    if (location.pathname === "/") {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    } else {
+      navigate("/");
+    }
+  };
+
   return (
     <header>
       <nav>
         <ul>
           <li>
-            <a href="#home">Home</a>
+            <a onClick={handleHomeClick} href="#home">
+              Home
+            </a>
             <a href="#skills">Skills</a>
             <a href="#projects">Projects</a>
 
