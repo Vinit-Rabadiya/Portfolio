202de779969f36640cb999d9731707d0bacc4157
vinit <vinitrabadiyasoft.en@gmail.com>
2026-03-09 14:08:49 +0300

    adding content for landing page home

--- personalPortfolio/src/pages/Home/Home.jsx
+++ personalPortfolio/src/pages/Home/Home.jsx
@@ -2,7 +2,37 @@ import React from "react";
 import "./Home.css";
 
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <div className="home-container">
+        <section
+          id="home"
+          className="min-h-screen flex flex-col justify-center items-center px-4"
+        >
+          <div className="hero-content text-center max-w-3xl">
+            <h1 className="text-5xl md:text-6xl font-bold mb-6">
+              Hi, I'm <span className="text-[#00ff88]">Vinit Rabadiya</span>
+              <span className="text-[#00ff88]">□</span>
+            </h1>
+            <p className="hero-tagline">Curiosity. Code. Create</p>
+            <h2 className="text-2xl md:text-3xl text-gray-400 mb-6">
+              Data Science Intern | Full Stack Developer
+            </h2>
+            <p className="hero=description">
+              Student developer exploring AI/ML and building web applications
+              while turning ideas into real projects.
+            </p>
+          </div>
+        </section>
+        <section id="skills">
+          <h2>Skills</h2>
+        </section>
+        <section id="projects">
+          <h2>Projects</h2>
+        </section>
+      </div>
+    </>
+  );
 }
 
 export default Home;
