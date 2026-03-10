d9592046be7d6be4373eb1613af5b752809f9bb7
vinit <vinitrabadiyasoft.en@gmail.com>
2026-03-10 23:09:49 +0300

    fixed glowing effect

--- personalPortfolio/src/pages/Home/Project.jsx
+++ personalPortfolio/src/pages/Home/Project.jsx
@@ -28,7 +28,7 @@ function Project() {
       <div className="projects-container grid grid-cols-[repeat(auto-fit,minmax(300px,320px))] justify-center gap-8 w-full">
         {projects.map((project, index) => (
           <div
-            className="project-card w-full rounded-xl overflow-hidden shadow-lg bg-[#111] hover:scale-105 hover:shadow-[0_0_20px_#00ff88] transition-all duration-300 p-4"
+            className="project-card w-full rounded-xl overflow-hidden shadow-lg bg-[#111] hover:scale-102 hover:shadow-[0_0_10px_#00ff88] transition-all duration-600 p-4"
             key={index}
           >
             <img
