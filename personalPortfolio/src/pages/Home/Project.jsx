35d78a2158ad14954ef7fd4a5992a38bf3cb3ef8
vinit <vinitrabadiyasoft.en@gmail.com>
2026-03-10 19:01:50 +0300

    fixed css for the card under projects

--- personalPortfolio/src/pages/Home/Project.jsx
+++ personalPortfolio/src/pages/Home/Project.jsx
@@ -6,7 +6,10 @@ function Project() {
   const projects = [
     {
       title: "Password Manager",
-      image: new URL("../../assets/password-manager.png", import.meta.url).href,
+      image: new URL(
+        "../../assets/ProjectImages/passwordManager.png",
+        import.meta.url,
+      ).href,
       description:
         "A secure password manager built with React and Firebase, allowing users to store and manage their passwords safely.",
       github: "https://github.com/Vinit-Rabadiya/Password-Manager",
@@ -18,16 +21,27 @@ function Project() {
       id="projects"
       className="min-h-screen flex flex-col justify-start items-center px-4 pt-10"
     >
-      <h2 className="text-4xl md:text-2xl font-bold text-center mb-5 -mt-10 text-[#00ff88]">
+      <h2 className="text-4xl md:text-2xl font-bold text-center mb-3 -mt-10 text-[#00ff88]">
         PROJECTS
       </h2>
-      <div className="projects-container">
+      <div className="projects-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {projects.map((project, index) => (
-          <div className="project-card" key={index}>
-            <img src={project.image} alt={project.title} />
-            <h3>{project.title}</h3>
-            <p>{project.description}</p>
-            <a href={project.github} target="_blank" rel="noopener noreferrer">
+          <div
+            className="project-card max-w-sm rounded overflow-hidden shadow-lg bg-[#111] hover:scale-105 transition-transform duration-300 p-4"
+            key={index}
+          >
+            <img
+              src={project.image}
+              alt={project.title}
+              className="w-full h-48 object-cover rounded"
+            />
+            <div className="py-4">
+              <div className="font-bold text-xl mb-3 text-white">
+                {project.title}
+              </div>
+              <p className="text-gray-400 text-base">{project.description}</p>
+            </div>
+            <a href={project.github} target="_blank" rel="noopener noreferrer" className="inline-block -mt-3">
               <img src={gitHubLogo} className="github-icon" />
             </a>
           </div>
